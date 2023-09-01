05994b614d300658297130ab084d88fa51e1e943
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-01 11:03:49 +0330

    Initial styels setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import type { Root } from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./styles/index.scss";
 
 const root: Root = createRoot(document.getElementById("root") as HTMLElement);
 
