154a8798e12330dc99b9de4387d89b23696d82b7
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-03 15:36:27 +0330

    Add the 'cta' section of the 'features' page

---
+++ src/containers/CTA/index.tsx
@@ -0,0 +1,54 @@
+import type { FC, ReactElement } from "react";
+import type { CTA as Props } from "../../types/Elements/CTA";
+import cn from "classnames";
+import SVG from "react-inlinesvg";
+import Container from "../../components/UI/Container";
+import Wrapper from "../../components/UI/Wrapper";
+import ContainerTitle from "../../components/UI/ContainerTitle";
+import Link from "../../components/UI/Link";
+import Style from "./style.module.scss";
+
+const CTA: FC<Props> = ({ title, subtitle, links, features }): ReactElement => (
+    <section className={Style.cta}>
+        <Container>
+            <Wrapper className={Style.wrapper}>
+                <ContainerTitle center={title.center} className={Style.cta_title}>
+                    { title.text }
+                </ContainerTitle>
+                { subtitle && (
+                    <h5 className={cn(Style.subtitle, subtitle.center && Style.center)}>{ subtitle.text }</h5>
+                )}
+                <div className={Style.buttons}>
+                    {
+                        links.map(({ id, link, subtext }) => (
+                            <div>
+                                <Link key={id}
+                                      to={link.url}
+                                      type={link.type}
+                                      variant={link.variant}
+                                      className={Style.btn_wide}>
+                                    { link.text }
+                                </Link>
+                                { subtext ? <p className={Style.subtext}>{ subtext }</p> : null }
+                            </div>
+                        ))
+                    }
+                </div>
+                { features && (
+                    <div className={Style.features}>
+                        {
+                            features.map(({ id, text, icon }) => (
+                                <div key={id} className={Style.feature}>
+                                    <SVG src={icon} className={Style.icon} />
+                                    <p className={Style.text}>{ text }</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                )}
+            </Wrapper>
+        </Container>
+    </section>
+);
+
+export default CTA;
--- src/pages/Features/index.tsx
+++ src/pages/Features/index.tsx
@@ -2,16 +2,18 @@ import type { FC, ReactElement } from "react";
 import type { FeaturesPageResources } from "../../types/Data/Pages/Features";
 import { useLoaderData } from "react-router-dom";
 import Hero from "../../containers/Hero";
+import CTA from "../../containers/CTA";
 import Sections from "./Sections";
 
 const Features: FC = (): ReactElement => {
 
-    const { hero } = useLoaderData() as FeaturesPageResources;
+    const { hero, cta } = useLoaderData() as FeaturesPageResources;
 
     return (
         <>
             <Hero {...hero} />
             <Sections />
+            <CTA {...cta} />
         </>
     );
 
