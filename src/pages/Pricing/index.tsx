c30bdb4473582283a4d808d8839887679d3df7cc
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-04 16:16:21 +0330

    Add the 'questions' section of the 'pricing' page

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -3,15 +3,17 @@ import type { PricingPageResources } from "../../types/Data/Pages/Pricing";
 import { useLoaderData } from "react-router-dom";
 import Plans from "../../containers/Plans";
 import CompareTable from "./compareTable";
+import Questions from "../../containers/Questions";
 
 const Pricing: FC = (): ReactElement => {
 
-    const { plans } = useLoaderData() as PricingPageResources;
+    const { plans, questions } = useLoaderData() as PricingPageResources;
 
     return (
         <>
             <Plans data={plans} />
             <CompareTable />
+            <Questions questions={questions} centerTitle={true} />
         </>
     );
 
