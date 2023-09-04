1fd061a7058920141ba6180be1ca843066738453
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-04 16:43:38 +0330

    Add the 'cta' section of the 'pricing' page

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -4,16 +4,18 @@ import { useLoaderData } from "react-router-dom";
 import Plans from "../../containers/Plans";
 import CompareTable from "./compareTable";
 import Questions from "../../containers/Questions";
+import CTA from "../../containers/CTA";
 
 const Pricing: FC = (): ReactElement => {
 
-    const { plans, questions } = useLoaderData() as PricingPageResources;
+    const { plans, questions, cta } = useLoaderData() as PricingPageResources;
 
     return (
         <>
             <Plans data={plans} />
             <CompareTable />
             <Questions questions={questions} centerTitle={true} />
+            <CTA {...cta} />
         </>
     );
 
